58dd3cb6641163af9d73ea5629b982e09fb384a7
fonget <26006683+fongap@users.noreply.github.com>
2026-08-19 15:24:01 +0800

    chore: stop tracking portable build artifacts

--- packaging/portable/target/release/delta-portable-launcher.d
+++
@@ -1,1 +0,0 @@
-D:\900\ AIWork\910\ GitHub\delta\packaging\portable\target\release\delta-portable-launcher.exe: D:\900\ AIWork\910\ GitHub\delta\packaging\portable\launcher\src\main.rs
--- packaging/portable/target/release/deps/delta_portable_launcher.d
+++
@@ -1,5 +0,0 @@
-D:\900 AIWork\910 GitHub\delta\packaging\portable\target\release\deps\delta_portable_launcher.d: src\main.rs
-
-D:\900 AIWork\910 GitHub\delta\packaging\portable\target\release\deps\delta_portable_launcher.exe: src\main.rs
-
-src\main.rs:
